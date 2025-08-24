1ce67720c8c23f9fcf6a0330d267da8c0e95225b
trilvm <trilvm2710@gmail.com>
2025-08-24 21:04:29 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ],
   css: [
     '~/assets/styles/main.scss'
   ],

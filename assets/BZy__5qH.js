c880c7041c04fcc508e44d0ab11a3aaccc8bdb14
trilvm <trilvm2710@gmail.com>
2025-08-24 18:52:12 +0700

    Updates

---
+++ assets/BZy__5qH.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./qOxbCsW6.js";import{C as n,o as s,w as f,I as p}from"./DW3w1KhX.js";const l={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{l as default};
---
+++ assets/Bv-u_Jsr.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,o as r,I as c}from"./DW3w1KhX.js";const n={};function t(e,a){return r(),s("code",null,[c(e.$slots,"default")])}const l=Object.assign(o(n,[["render",t]]),{__name:"ProseCode"});export{l as default};
